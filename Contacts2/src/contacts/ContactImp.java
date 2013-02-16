6914a4d8d64d6cfe54a1d3c1a8ff9dac84f9d2cd
Bri <briannelmurphy@gmail.com>
2013-02-16 18:26:51 +0000

    ARGHHH!!!!!!!!

---
+++ Contacts2/src/contacts/ContactImp.java
@@ -0,0 +1,38 @@
+
+import java.util.ArrayList;
+
+public class ContactImp implements Contact {
+
+	int contactId;
+	String contactName;
+	String contactNotes = "";// = new ArrayList<String>();
+
+	public ContactImp(String name, int id){
+		contactName = name;
+		contactId = id;
+	}
+	
+	@Override
+	public int getId() {
+		return contactId;
+	}
+
+	@Override
+	public String getName() {
+		return contactName;
+	}
+
+	@Override
+	public String getNotes() {
+		return contactNotes.toString().replace("[", "").replace("]", "");
+	}
+
+	@Override
+	public void addNotes(String note) {
+		if(contactNotes.isEmpty()){
+			contactNotes = note.trim();
+		}else
+			contactNotes += note.trim();
+	}
+
+}
